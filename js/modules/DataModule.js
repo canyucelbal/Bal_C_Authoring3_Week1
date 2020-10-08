71cb24087320d389b454a9e4faee92e746f893cc
canyucelbal <c_bal@fanshaweonline.ca>
2020-10-08 21:17:32 +0300

    adding dynamic data to out view

--- js/modules/DataModule.js
+++ js/modules/DataModule.js
@@ -1,5 +1,32 @@
 export default {
-    name: "Trevor",
-    role: "Prof",
-    nickname: "Tervor"
+    Justin: {
+        name: "Justin",
+        role: "Coordinator",
+        nickname: "I don't know"
+    },
+
+    John: {
+        name: "John",
+        role: "super chill prof",
+        nickname: "just john"
+    },
+
+    Joe: {
+        name: "Joe",
+        role: "Prof",
+        nickname: "Teddy Bear"
+    },
+
+    Pan: {
+        name: "Pan",
+        role: "Code Ninja",
+        nickname: "Spider Pan"
+    },
+
+    Jarrod: {
+        name: "Jarrod",
+        role: "Male Model Prof",
+        nickname: "Zoolander"
+    }
+    
 }

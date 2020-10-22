558442a71007246d268838f1c17adfe2e50106ac
canyucelbal <c_bal@fanshaweonline.ca>
2020-10-22 20:20:54 +0300

    done with writing fetch

--- js/main.js
+++ js/main.js
@@ -1,34 +1,37 @@
-// import your packages here
-import Team from "./modules/DataModule.js";
-
 (() => {
-    // stub * just a place for non component specific stuff
-    let userSection = document.querySelector(".user-section"),
-        userTemplate = document.querySelector("#profs-template").content;
+    // start with a Fetch all
+    fetch('./DataSet.json')
+        .then(res => res.json())  // parse the JSON (translate) back to plain JS
+        .then(data => {
+            // this is our data (DataSet.json)
+            //converted to a plain JavaScript object
+            handleDataSet(data);
+        })
+    .catch((error) => console.log(error));
 
-    debugger;
 
-    // select our user elements and load the content
-
+    // this receives the data payload from our AJAX request, parses it (turns the returned JSON object back into a plain JavaScript object) and renders the data to our view (the markup in index.html)
     function handleDataSet(data) {
-       
+        let userSection = document.querySelector('.user-section'),
+            userTemplate = document.querySelector('#user-template').content;
+
+        debugger;
+
+        // loop through the JavaScript object and for each user, make a copy of the user template we find at the bottom of index.html, populate it with the user's data, and put that fresh copy in the users section in index.html
 
         for (let user in data) {
-
-            // make a copy of our template here and then
-            // populate the children (text elements) with 
-            // the static data from the Team object
             let currentUser = userTemplate.cloneNode(true),
                 currentUserText = currentUser.querySelector('.user').children;
 
-            currentUserText[1].textContent = data[user].name;
-            currentUserText[2].textContent = data[user].role;
-            currentUserText[3].textContent = data[user].nickname;
+            currentUserText[1].src = `images/${data[user].avatar}.jpg`;
+            currentUserText[2].textContent = data[user].name;
+            currentUserText[3].textContent = data[user].role;
+            currentUserText[4].textContent = data[user].nickname;
 
+            // add this new user to the view
             userSection.appendChild(currentUser);
         }
-    }
 
-    handleDataSet(Team);
-    
+        console.log(data);
+    }
 })();
